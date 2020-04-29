b9c2da1d991ccaa66b8bde971107737fc905ab6d
github username <github email address>
2020-04-28 20:06:21 -0500

    started home page styling

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,36 @@
-import React, { Component } from 'react';
-import SignUp from './components/authorization/SignUp';
-import Login from './components/authorization/Login';
+import React, { Component } from "react";
+import SignUp from "./components/authorization/SignUp";
+import Login from "./components/authorization/Login";
 
 class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                <h2>This is the Home Page</h2>
-                <Login handleLogin={this.props.handleLogin} routerProps={this.props.routerProps}/>
-                <SignUp handleLogin={this.props.handleLogin} routerProps={this.props.routerProps}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <div className="welcome">Welcome to Group Gift</div>
+          <div className="giving">
+            Giving a Group Gift has never been easier!
+          </div>
+          <ul className='home-ul'>
+            <li className='login-li'>
+              <Login
+                handleLogin={this.props.handleLogin}
+                routerProps={this.props.routerProps}
+              />
+            </li>
+            <li className='signup-li'>
+              <SignUp
+                handleLogin={this.props.handleLogin}
+                routerProps={this.props.routerProps}
+              />
+            </li>
+          </ul>
+        </header>
+        <div className="background"><div className='how-it-works'></div></div>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
