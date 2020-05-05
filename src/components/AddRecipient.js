733463ca1eb67bbe54ab7fc92a78ec3b690356bd
github username <github email address>
2020-05-05 12:02:26 -0500

    add date picker and hover add gift btn

--- src/components/AddRecipient.js
+++ src/components/AddRecipient.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class AddRecipient extends Component {
   state = {
     recipient: "",
-    eventDate: "",
+    eventDate: '',
     relationship: "",
     budget: "",
     interests: "",
@@ -28,6 +30,12 @@ class AddRecipient extends Component {
         });
       });
   }
+
+  handleChangeDate = (date) => {
+    this.setState({
+      eventDate: date,
+    });
+  };
 
   handleChange = (event) => {
     this.setState({
@@ -100,94 +108,118 @@ class AddRecipient extends Component {
           handleLogout={this.props.handleLogout}
           routerProps={this.props.routerProps}
         />
-        <div className='add-recipient-group-name'>Group: {this.state.group.name}</div>
-        <div className='directions-page-recipient'>Add at least one recipient that your group will be giving a gift to.  You can add more than one person in the name input-field or add each person individually.</div>
+        <div className="add-recipient-group-name">
+          Group: {this.state.group.name}
+        </div>
+        <div className="directions-page-recipient">
+          Add at least one recipient that your group will be giving a gift to.
+          You can add more than one person in the name input-field or add each
+          person individually.
+        </div>
         {this.state.recipients.length > 0 ? (
           this.state.recipients.map((recipient, idx) => (
-            <div key={idx} className='added-recipient'>
-              <span className='name'>{recipient.recipient}</span>
-              <span className='date'>Date of Event: {recipient.eventDate}</span>
-              <span className='relationship'>Related: {recipient.relationship}</span>
-              <div className='budget'>Budget: {recipient.budget}</div>
-              <div className='interests'>{recipient.interests}</div>
-              <div className='notes'>{recipient.notes}</div>
+            <div key={idx} className="added-recipient">
+              <span className="name">{recipient.recipient}</span>
+              <span className="date">Date of Event: {recipient.eventDate}</span>
+              <span className="relationship">
+                Related: {recipient.relationship}
+              </span>
+              <div className="budget">Budget: {recipient.budget}</div>
+              <div className="interests">{recipient.interests}</div>
+              <div className="notes">{recipient.notes}</div>
             </div>
           ))
         ) : (
           <div></div>
         )}
-        <div className='form-recipient'>
+
+        <div className="form-recipient">
           <form>
-            <label className='who'>
+            <label className="who">
               Who is this gift for?
               <input
                 type="text"
-                className='who'
+                className="who"
                 name="recipient"
                 value={this.state.recipient}
                 onChange={this.handleChange}
               />
             </label>
-            <label className='date'>
+            <label className="date">
               Enter a date of the event
-              <input
+              <div>
+                <DatePicker
+                  selected={this.state.eventDate}
+                  onChange={this.handleChangeDate}
+                />
+              </div>
+              {/* <input
                 type="text"
                 className='date'
                 name="eventDate"
                 value={this.state.eventDate}
                 onChange={this.handleChange}
-              />
+              /> */}
             </label>
-            <label className='related'>
+            <label className="related">
               How is this recipient related to your group?
               <input
                 type="text"
-                className='related'
+                className="related"
                 name="relationship"
                 value={this.state.relationship}
                 onChange={this.handleChange}
               />
             </label>
-            <label className='budget'>
+            <label className="budget">
               Budget for group gift
               <input
                 type="number"
-                className='budget'
+                className="budget"
                 name="budget"
                 value={this.state.budget}
                 onChange={this.handleChange}
               />
             </label>
-            <label className='hobbies'>
+            <label className="hobbies">
               List any interests or hobbies this person has
               <textarea
                 name="interests"
-                className='hobbies'
+                className="hobbies"
                 value={this.state.interests}
                 onChange={this.handleChange}
               />
             </label>
-            <label className='add-recipient-notes'>
+            <label className="add-recipient-notes">
               Notes
               <textarea
                 name="notes"
-                className='add-recipient-notes'
+                className="add-recipient-notes"
                 value={this.state.notes}
                 onChange={this.handleChange}
               />
             </label>
           </form>
-          <div className='add-another-directions'>If you would like to add another recipient for this group click below</div>
-          <button onClick={this.handleAddAnotherRecipient} className='add-another'>
+          <div className="add-another-directions">
+            If you would like to add another recipient for this group click
+            below
+          </div>
+          <button
+            onClick={this.handleAddAnotherRecipient}
+            className="add-another"
+          >
             Add another recipient
           </button>
-          <button onClick={this.handleSubmitRecipients} className='submit-recipient'>
+          <button
+            onClick={this.handleSubmitRecipients}
+            className="submit-recipient"
+          >
             Add Recipient(s) to Group
           </button>
         </div>
         <div className="recipient-page-gift">
-            <img src="../../christmas-presents.png" alt="gift-icon" width={130} />
-          </div>
+          <img src="../../christmas-presents.png" alt="gift-icon" width={130} />
+        </div>
       </div>
     );
   }
